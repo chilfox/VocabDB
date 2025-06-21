cdac1b9657db56ed0be3074204728301ec35b144
cwh555 <bergerchen719@gmail.com>
2025-06-21 11:02:30 +0800

    go into word or label

--- englishvocabdatabase/lib/logic/page/pageInformation.dart
+++ englishvocabdatabase/lib/logic/page/pageInformation.dart
@@ -4,9 +4,25 @@ import '../output/outputListNotifier.dart';
 PageInformation pageStatus = PageInformation();
 
 class PageInformation {
-  NotifierType _type;
+  late NotifierType _type;  //the type of listviewer
+  late bool _insideLabel;
+  late int _labelId;
+  late bool _insideWord;
+  late int _wordId;
 
-  PageInformation({NotifierType type = NotifierType.Label}) : _type = type;
+  PageInformation(){
+    _type = NotifierType.Label;
+    _insideLabel = false;
+    _insideWord = false;
+    _labelId = -1;
+    _wordId = -1;
+  }
+
+  //getter
+  bool get insideLabel => _insideLabel;
+  bool get insideword => _insideWord;
+  int get labelId => _labelId;
+  int get wordId => _wordId;
 
   void setNotifierType(NotifierType type){
     _type = type;
@@ -17,5 +33,23 @@ class PageInformation {
     return _type;
   }
 
+  void goIntoLabel({required int id}){
+    _insideLabel = true;
+    _labelId = id;
+  }
 
+  void goOutLabel(){
+    _insideLabel = false;
+    _labelId = -1;
+  }
+
+  void goIntoWord({required int id}){
+    _insideWord = true;
+    _wordId = id;
+  }
+
+  void goOutWord(){
+    _insideWord = false;
+    _wordId = -1;
+  }
 }
