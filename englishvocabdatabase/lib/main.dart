caa9c2f8e6b218f44dde36b25219b3ce686b951a
cwh555 <bergerchen719@gmail.com>
2025-07-01 14:55:31 +0800

    add sharing to main

--- englishvocabdatabase/lib/main.dart
+++ englishvocabdatabase/lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:englishvocabdatabase/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:englishvocabdatabase/share/share.dart';
+import 'package:englishvocabdatabase/share/processShare.dart';
+import 'package:englishvocabdatabase/share/observer.dart';
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  //handle share text when open the app
+  await ShareHandler.instance.init();
+  ShareHandler.instance.processPending((text){
+    //process the shared text
+    debugPrint('處理分享文字: $text');
+    ShareProcessor.processText(text);
+  });
+
   runApp(const ProviderScope(child: MyApp()));
+  
+  //前台處理分享字串
+  final share_observer = ShareObserver();
+  share_observer.init();
 }
 
 class MyApp extends ConsumerWidget {
