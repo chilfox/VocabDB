028f16c8ac26fffdb2422af2afbe4cae2ee7a20c
chilfox <chilfox06@gmail.com>
2025-07-02 16:55:33 +0800

    Add add word page in labe

---
+++ englishvocabdatabase/lib/pages/BrowsePage/InLabelWordListPage/in_label_add_word_search_bar.dart
@@ -0,0 +1,53 @@
+import 'package:englishvocabdatabase/logic/output/outputItem.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../logic/output/outputListNotifier.dart';
+
+class InLabelAddWordSearchBar extends ConsumerStatefulWidget {
+  final OutputListItem label;
+
+  const InLabelAddWordSearchBar({super.key, required this.label});
+
+  @override
+  ConsumerState<InLabelAddWordSearchBar> createState() => _InLabelAddWordSearchBarState();
+}
+
+class _InLabelAddWordSearchBarState extends ConsumerState<InLabelAddWordSearchBar> {
+  OutputListNotifier? service;
+  final TextEditingController textController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return TextField(
+      controller: textController,
+      decoration: InputDecoration(
+        hintText: 'Search Word not in the Label',
+        prefixIcon: const Icon(Icons.search),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.clear),
+          onPressed: () {
+            textController.clear();
+          },
+        ),
+        border: const OutlineInputBorder(),
+      ),
+      keyboardType: TextInputType.text,
+      onChanged: (text) async {       
+        final service = ref.read(outputListNotifierProvider(NotifierType.Word, inlabel: true, labelId: widget.label.id).notifier);
+        bool? success = await service.searchNotInLabel(text, widget.label.id);
+        if (!context.mounted) return;
+        if (!success) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Search failed')),
+          );
+        }
+      }
+    );
+  }
+}
