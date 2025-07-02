f0dee8c44c02217758284f58a596b7b1ddd8821c
cwh555 <bergerchen719@gmail.com>
2025-07-02 17:11:01 +0800

    floating window prototype

--- englishvocabdatabase/android/app/src/main/kotlin/com/example/englishvocabdatabase/MainActivity.kt
+++ englishvocabdatabase/android/app/src/main/kotlin/com/example/englishvocabdatabase/MainActivity.kt
@@ -1,5 +1,28 @@
 package com.example.englishvocabdatabase
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private lateinit var floatingWindowManager: FloatingWindowManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        floatingWindowManager = FloatingWindowManager(this)
+        handleProcessTextIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleProcessTextIntent(intent)
+    }
+
+    private fun handleProcessTextIntent(intent: Intent) {
+        if (Intent.ACTION_PROCESS_TEXT == intent.action) {
+            val receivedText = intent.getStringExtra(Intent.EXTRA_PROCESS_TEXT)
+            floatingWindowManager.showFloatingWindow(receivedText ?: "未收到文字")
+        }
+    }
+}
