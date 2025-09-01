8c396e66ac65d5d8beb8941db8811fef1eb42009
cwh555 <bergerchen719@gmail.com>
2025-09-01 13:40:49 +0800

    change com.example.xxx to com.skfoxwh

--- englishvocabdatabase/android/app/build.gradle.kts
+++ englishvocabdatabase/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 
 
 android {
-    namespace = "com.example.englishvocabdatabase"
+    namespace = "com.skfoxwh.englishvocabdatabase"
     compileSdk = flutter.compileSdkVersion
     //change version
     ndkVersion = "27.0.12077973"
@@ -32,7 +32,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.englishvocabdatabase"
+        applicationId = "com.skfoxwh.englishvocabdatabase"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
